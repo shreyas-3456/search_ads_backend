b357ad6b09d4b7e0e02c626ee85df88aa243d1a2
Shreyas <shreyas.nigam25@gmail.com>
2024-05-21 23:49:12 +0530

    abc

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const connectDB = (url) => {
-	mongoose.set('strictQuery', false);
-	return mongoose.connect(url);
-};
+  mongoose.set('strictQuery', false)
+  return mongoose.connect(url)
+}
 
-module.exports = connectDB;
+module.exports = connectDB
+
+//abc
